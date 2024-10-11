02d11449d160ac445ee137f186b48d60acf09d6d
baesee <anekddl7@naver.com>
2024-10-11 14:34:08 +0900

    카드 스와이프 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import { Box } from '@mui/material';
+import { Box, CssBaseline } from '@mui/material';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
 function App() {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                height: '100vh', // 뷰포트 높이로 설정
-                maxWidth: '500px',
-                margin: '0 auto',
-                bgcolor: 'background.paper',
-                overflow: 'hidden', // 전체 레이아웃의 오버플로우를 숨김
-            }}
-        >
-            <Header />
-            <MainContent />
-            <Footer />
-        </Box>
+        <>
+            <CssBaseline />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100vh',
+                    maxWidth: '500px',
+                    margin: '0 auto',
+                    bgcolor: 'background.paper',
+                    overflow: 'hidden',
+                }}
+            >
+                <Header />
+                <MainContent />
+                <Footer />
+            </Box>
+        </>
     );
 }
 
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,22 +1,101 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
+import { CardData, CardEvent, CardSwiper } from 'react-card-swiper';
+
+import EmptyState from '../assets/icons/empty-state.svg';
+import hulu from '../assets/images/hulu.jpeg';
+import instagram from '../assets/images/instagram.png';
+import spotify from '../assets/images/spotify.png';
+
+const Content = () => (
+    <Typography px={2} variant="h6">
+        Lorem ipsum dolor sit amet.
+    </Typography>
+);
+
+const mockData: CardData[] = [
+    {
+        id: '88552078',
+        meta: { apk: 'some-apk-a.apk' },
+        src: hulu,
+        content: <Content />,
+    },
+    {
+        id: 'fc7e0bd4',
+        meta: { apk: 'some-apk-b.apk' },
+        src: instagram,
+        content: <Content />,
+    },
+    {
+        id: 'da9a7067',
+        meta: { apk: 'some-apk-c.apk' },
+        src: spotify,
+        content: <Content />,
+    },
+];
 
 const MainContent: React.FC = () => {
+    const handleDismiss: CardEvent = (el, meta, id, action, operation) => {
+        console.log({ el, meta, id, action, operation }); // event data to be handled
+    };
+
+    const handleFinish = (status: string) => {
+        console.log(status); // 'finished'
+    };
+
     return (
         <Box
             component="main"
             sx={{
                 flexGrow: 1,
-                overflow: 'auto', // 내용이 많아질 경우 스크롤 생성
+                overflow: 'hidden',
                 display: 'flex',
                 flexDirection: 'column',
-                padding: 2, // 내용과 경계 사이에 여백 추가
+                padding: 2,
             }}
         >
-            <Typography variant="h4" component="h1" gutterBottom>
-                메인 컨텐츠
-            </Typography>
-            {/* 여기에 메인 컨텐츠를 추가하세요 */}
+            <Stack
+                height={'90%'}
+                width={'90%'}
+                direction="column"
+                alignItems="center"
+                justifyContent={'center'}
+                p={2}
+            >
+                <CardSwiper
+                    data={mockData}
+                    onFinish={handleFinish}
+                    onDismiss={handleDismiss}
+                    dislikeButton={<div>Left</div>}
+                    likeButton={<div>Right</div>}
+                    withRibbons
+                    likeRibbonText="INSTALL"
+                    dislikeRibbonText="PASS"
+                    ribbonColors={{
+                        bgLike: 'green',
+                        bgDislike: 'red',
+                        textColor: 'white',
+                    }}
+                    emptyState={
+                        <Stack
+                            direction={'column'}
+                            alignItems={'center'}
+                            justifyContent={'center'}
+                            textAlign={'center'}
+                            gap={2}
+                        >
+                            <Box
+                                component={'img'}
+                                width={250}
+                                src={EmptyState}
+                            />
+                            <Typography variant={'subtitle2'}>
+                                You've reached the <br /> end of the list
+                            </Typography>
+                        </Stack>
+                    }
+                />
+            </Stack>
         </Box>
     );
 };
