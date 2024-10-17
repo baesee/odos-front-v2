a3cc4ea511e08f685c0939cc7b2741698afa71ed
baesee <anekddl7@naver.com>
2024-10-17 17:50:31 +0900

    PWA 관련 추가 설정

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -12,14 +12,20 @@ type Config = {
 };
 
 export function register(config?: Config) {
-    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-        const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    console.log(import.meta.env.VITE_API_URL);
+    if (import.meta.env.PROD && 'serviceWorker' in navigator) {
+        const publicUrl = new URL(
+            import.meta.env.VITE_API_URL + import.meta.env.BASE_URL,
+            window.location.href
+        );
         if (publicUrl.origin !== window.location.origin) {
             return;
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+            const swUrl = `${
+                import.meta.env.VITE_API_URL + import.meta.env.BASE_URL
+            }service-worker.js`;
 
             if (isLocalhost) {
                 checkValidServiceWorker(swUrl, config);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                clientsClaim: true,
+                skipWaiting: true,
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
     server: {
         host: '0.0.0.0',
         port: 3000,
