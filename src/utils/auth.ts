be19ee97457a626deeea2fbcee64e23a5db39c36
baesee <anekddl7@naver.com>
2024-10-25 17:27:45 +0900

    운영 환경 로그아웃 가능 토록 쿠키 처리

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,12 +3,21 @@ import { socialLogout } from '../api/authApi';
 
 export const logout = async () => {
     try {
-        await socialLogout(Cookies.get('odos_refresh_token') || ''); // authApi의 Logout 함수 호출
+        await socialLogout(Cookies.get('odos_refresh_token') || '');
     } catch (error) {
         console.error('Logout API 호출 중 오류 발생:', error);
     } finally {
-        // API 호출 성공 여부와 관계없이 항상 로컬 쿠키를 삭제
-        Cookies.remove('odos_access_token');
-        Cookies.remove('odos_refresh_token');
+        Cookies.remove('odos_access_token', {
+            path: '/',
+            domain: 'odos.today',
+            secure: true,
+            sameSite: 'none',
+        });
+        Cookies.remove('odos_refresh_token', {
+            path: '/',
+            domain: 'odos.today',
+            secure: true,
+            sameSite: 'none',
+        });
     }
 };
