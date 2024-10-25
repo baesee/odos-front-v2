cba87383f93d1469469f3cfed3beac5a2a1f99d3
baesee <anekddl7@naver.com>
2024-10-25 15:38:17 +0900

    유투브 임베디드 삽입

--- src/components/card-swiper/CardSwiperContent.tsx
+++ src/components/card-swiper/CardSwiperContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
+import YouTubeEmbed from './YouTubeEmbed';
 
 interface CardSwiperContentProps {
     title: string;
@@ -17,56 +18,71 @@ const CardSwiperContent: React.FC<CardSwiperContentProps> = ({
     representativeTag,
 }) => {
     return (
-        <Box
-            sx={{
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                maxHeight: '30%', // 카드의 최대 40%까지만 차지하도록 설정
-                padding: 2,
-                background: 'rgba(0, 0, 0, 0.3)',
-                color: 'white',
-                backdropFilter: 'blur(5px)',
-                overflowY: 'auto', // 세로 스크롤 추가
-                '&::-webkit-scrollbar': {
-                    width: '0.4em',
-                },
-                '&::-webkit-scrollbar-track': {
-                    boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
-                    webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
-                },
-                '&::-webkit-scrollbar-thumb': {
-                    backgroundColor: 'rgba(255,255,255,.1)',
-                    outline: '1px solid slategrey',
-                },
-            }}
-        >
+        <>
+            {videoLink && (
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <YouTubeEmbed videoId={videoLink} />
+                </Box>
+            )}
             <Box
                 sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    maxHeight: '30%',
+                    padding: 2,
+                    background: 'rgba(0, 0, 0, 0.3)',
+                    color: 'white',
+                    backdropFilter: 'blur(5px)',
+                    overflowY: 'auto',
+                    '&::-webkit-scrollbar': {
+                        width: '0.4em',
+                    },
+                    '&::-webkit-scrollbar-track': {
+                        boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+                        webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                        backgroundColor: 'rgba(255,255,255,.1)',
+                        outline: '1px solid slategrey',
+                    },
                 }}
             >
-                <Typography
-                    variant="h5"
-                    sx={{ fontFamily: 'NanumSquareRoundEB, sans-serif' }}
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}
                 >
-                    {title}
-                </Typography>
-                {videoLink && (
-                    <Typography variant="body2" sx={{ ml: 1 }}>
-                        {videoSource}
+                    <Typography
+                        variant="h5"
+                        sx={{ fontFamily: 'NanumSquareRoundEB, sans-serif' }}
+                    >
+                        {title}
+                    </Typography>
+                    {videoLink && (
+                        <Typography variant="body2" sx={{ ml: 1 }}>
+                            {videoSource}
+                        </Typography>
+                    )}
+                </Box>
+                {!videoLink && (
+                    <Typography variant="body1" sx={{ mt: 1 }}>
+                        {description}
                     </Typography>
                 )}
             </Box>
-            {!videoLink && (
-                <Typography variant="body1" sx={{ mt: 1 }}>
-                    {description}
-                </Typography>
-            )}
-        </Box>
+        </>
     );
 };
 
